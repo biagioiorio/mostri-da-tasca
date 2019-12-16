2e35e1a61bf8d14e6a38f769eb292ea25b6069c0
biagioiorio <biorio06@gmail.com>
2019-12-16 21:19:30 +0100

    SessionId funziona. È una proprietà della main activity.

--- app/src/main/java/com/example/mostridatasca/MainActivity.java
+++ app/src/main/java/com/example/mostridatasca/MainActivity.java
@@ -7,8 +7,21 @@ import android.os.Bundle;
 import android.se.omapi.Session;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity{
+
+    private String sessionId;
+
+    public static final String BASE_URL = "https://ewserver.di.unimi.it/mobicomp/mostri/";
     public static final String SHARED_PREFS_NAME = "sharedPrefs";   // Nome delle SharedPreferences
     public static final String SESSION_ID_KEY = "sessionId";    // Chiave del session_id
 
@@ -18,43 +31,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("MainActivity", "OnCreate");
-        getSessionId();
+        setSessionId();
     }
 
 
-    public void saveSessionId() {
+    public void setSessionId() {
+        /**
+         * Setta il valore del session_id con il valore memorizzato nelle SharedPreferences
+         * con la chiave SESSION_ID_KEY. Se è vuoto dire che l'utente è nuovo:
+         * faccio una chiamata di rete (nel metodo setSessionIdFromServer())
+         * al server per ottenere il session_id e lo salvo nelle SharedPreferences
+         */
+
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS_NAME, MODE_PRIVATE);
+        this.sessionId = sharedPreferences.getString(SESSION_ID_KEY, "");
+
+        if (this.sessionId.isEmpty()) { // Nuovo utente
+            Log.d("MainActivity", "SessionId non presente => nuovo utente");
+            setSessionIdFromServer();
+        }
+    }
+
+
+    public void saveSessionId(String sessionId) {
         /**
          * @author Biagio Iorio
-         * Metodo che salva in modo persistente il session_id dell'utente,
+         * Salva in modo persistente il session_id dell'utente,
          * se non è già presente, nelle SharedPreferences
          */
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS_NAME, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        // editor.putString(SESSION_ID_KEY, "sessionid2");
-
-        String sessionId = sharedPreferences.getString(SESSION_ID_KEY, "");
-        // editor.remove(SESSION_ID_KEY);
+        editor.putString(SESSION_ID_KEY, sessionId);
         editor.apply();
-        Log.d("MainActivity", String.valueOf(sessionId.isEmpty()));
+        Log.d("MainActivity", "session_id salvato nelle sharedPreferences.");
     }
 
-    public String getSessionId() {
-        /**
-         * Ritorna il valore del session_id memorizzato nelle SharedPreferences
-         * con la chiave SESSION_ID_KEY. Se non è presente vuol dire che l'utente è nuovo:
-         * faccio una chiamata di rete (register) al server per ottenere il session_id
-         */
 
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS_NAME, MODE_PRIVATE);
-        String sessionId = sharedPreferences.getString(SESSION_ID_KEY, "");
+    private void setSessionIdFromServer() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = BASE_URL + "register.php";
 
-        if(sessionId.isEmpty()){
-            // Nuovo utente
-            Log.d("MainActivity", "SessionId non presente => nuovo utente");
-            // Fai chiamata register per ricevere il session id e salvalo nelle shared preferences
-        }
+        // prepare the request
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
 
-        return sessionId;
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Log.d("MainActivity", "Response: " + response.toString());
+                        try {
+                            sessionId = response.getString("session_id");
+                            saveSessionId(sessionId);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Log.d("MainActivity", "session_id: " + sessionId);
+                    }
+                },
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("MainActivity", "Errore");
+                        // TODO: gestisci l'errore
+                    }
+                }
+        );
+
+        // add it to the RequestQueue
+        queue.add(getRequest);
     }
+
 }
